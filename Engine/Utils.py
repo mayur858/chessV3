6e10a62439cc9562f24a771d34d117824eadf5f1
MSI\rohit <rohitkaravadra1997@gmail.com>
2024-03-03 13:29:20 +0530

    UI Panels Added to the Engine

--- Engine/Utils.py
+++ Engine/Utils.py
@@ -1,6 +1,8 @@
 import pygame
 from Engine.Shared import Shared
 
+pygame.font.init()
+
 
 def pos_between(pos_1, pos_2, check_pos) -> bool:
     pos1 = pygame.Vector2(pos_1)
@@ -11,21 +13,51 @@ def pos_between(pos_1, pos_2, check_pos) -> bool:
     return False
 
 
-def format_text(text, color=pygame.Color("white"), size=10, text_type="Times New Roman"):
-    """function to generate text surface"""
-    pygame.font.init()
-    text_formate = pygame.font.SysFont(text_type, size)
-    text_surface = text_formate.render(text, True, color)
-    return text_surface
-
-
 def get_size_from_percent(value: tuple) -> tuple:
     width = (Shared.display_resolution[0]/100) * value[0]
     height = (Shared.display_resolution[1]/100) * value[1]
     return (width, height)
 
 
-def get_center_anchor(value: tuple) -> tuple:
-    xpos = (Shared.display_resolution[0] - value[0]) / 2
-    ypos = (Shared.display_resolution[1] - value[1]) / 2
+def get_relative_size(target: tuple, destination: tuple) -> tuple:
+    width = (destination[0] / 100) * target[0]
+    height = (destination[1] / 100) * target[1]
+    return (width, height)
+
+
+def get_center_anchor_pos(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = (destination.width/2) + destination.topleft[0]
+    ypos = (destination.height/2) + destination.topleft[1]
+    return (xpos, ypos)
+
+
+def get_center_anchor_offset(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = (destination.width - target.width)/2 + \
+        destination.topleft[0]-target.topleft[0]
+    ypos = (destination.height - target.height)/2 + \
+        destination.topleft[1]-target.topleft[1]
+    return (xpos, ypos)
+
+
+def get_leftcenter_anchor_pos(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = destination.left + target.width/2
+    ypos = destination.center[1]
+    return (xpos, ypos)
+
+
+def get_rightcenter_anchor_pos(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = destination.right - target.width/2
+    ypos = destination.center[1]
+    return (xpos, ypos)
+
+
+def get_topleft_anchor_pos(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = destination.topleft[0] + target.width/2
+    ypos = destination.topleft[1] + target.height/2
+    return (xpos, ypos)
+
+
+def get_topleft_anchor_offset(target: pygame.Rect, destination: pygame.Rect) -> tuple:
+    xpos = destination.topleft[0] + target.width/2 - target.topleft[0]
+    ypos = destination.topleft[1] + target.height/2 - target.topleft[1]
     return (xpos, ypos)
--- Scenes/MenuScene.py
+++ Scenes/MenuScene.py
@@ -1,18 +1,19 @@
 from Engine import Utils
 from Engine.SceneManager import *
 from Engine.App import App
+from Engine import UI
 import pygame
 
 
 class MenuScene(Scene):
     def __init__(self):
         super().__init__("menu")
-        self.hint = Utils.format_text("Press Enter To Start",
-                                      pygame.Color("black"), 50)
-        self.hintRect = self.hint.get_rect()
-
-        delta = Utils.get_center_anchor(self.hintRect.size)
-        self.hintRect = self.hintRect.move(delta)
+        self.panel = UI.Panel((100, 100), self.display,
+                              "center")
+        self.hint = UI.TextBox("Press Enter To start", 50)
+        self.hint.rect.center = Utils.get_center_anchor_pos(
+            self.hint.rect, self.panel.rect)
+        self.panel.add_child(self.hint)
 
     def update(self):
         for event in App.inputs:
@@ -20,4 +21,4 @@ class MenuScene(Scene):
                 if event.key == pygame.K_RETURN:
                     SceneManager.change_scene("game")
 
-        self.display.blit(self.hint, self.hintRect)
+        self.panel.update()
